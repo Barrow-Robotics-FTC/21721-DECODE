0164775852be933fbc9f1f61b256ff57f078d936
MikeyIsANerd <mp4callofdutymaster@gmail.com>
2025-12-17 12:58:49 -0500

    established launcher utility v2 file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/LauncherV2.java
@@ -0,0 +1,169 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+public class LauncherV2 {
+    // Launcher constants
+    public int CLOSE_TARGET_RPM = 1050;
+    public int FAR_TARGET_RPM = 1400;
+    public int TARGET_RPM = 1300;
+
+    final int RPM_TOLERANCE = 50;
+    final int RPM_IN_RANGE_TIME = 250;
+    final int MIN_TIME_BETWEEN_LAUNCHES = 500;
+    public final double feedPower = .8;
+    public final double feedPowerSwapped = -.8;
+
+    final int FEED_TIME = 1200;
+
+    public boolean launched = false;
+
+    // Motors and servos
+    public static DcMotorEx chipMotor;
+    public static CRServo lServo;
+    public static CRServo rServo;
+
+    // Other variables
+    public State state = State.IDLE;
+    private int launches;
+
+    public int targetLaunches;
+    public boolean isBusy;
+
+    // timers
+    private final ElapsedTime feedTimer = new ElapsedTime();
+    private final ElapsedTime inToleranceTimer = new ElapsedTime();
+    private final ElapsedTime recoveryTimer = new ElapsedTime();
+
+
+    public LauncherV2(HardwareMap hardwareMap) {
+        // initialize hardware (drivetrain is initialized by Pedro Pathing)
+        chipMotor = hardwareMap.get(DcMotorEx.class, "chipper");
+        lServo = hardwareMap.get(CRServo.class, "lServo");
+        rServo = hardwareMap.get(CRServo.class, "rServo");
+
+        // Set launcher motor characteristics
+        chipMotor.setZeroPowerBehavior(BRAKE);
+        chipMotor.setMode(com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER);
+        chipMotor.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300,0,0,10));
+        chipMotor.setDirection(DcMotorEx.Direction.FORWARD);
+
+        // Make sure servos are stopped
+    }
+
+    public enum State {
+        IDLE, //waiting to launch
+        SPEED_UP,
+        FEED,
+        RECOVER
+    }
+
+    public void stop() {
+        state = State.IDLE;
+        chipMotor.setPower(0);
+        lServo.setPower(0);
+        rServo.setPower(0);
+        launches = 0;
+    }
+
+    public boolean isBusy() {
+        State currentState = getState();
+        if (currentState == State.IDLE) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+
+    public double getChipRPM() {
+        return chipMotor.getVelocity();
+    }
+
+    public int getLaunches() {
+        return launches;
+    }
+
+    public int getTargetRPM() {
+        return TARGET_RPM;
+    }
+
+    public void setTargetLaunches(int launches) {
+        this.targetLaunches = launches;
+    }
+
+
+
+    public void launch() {
+        if (state == State.IDLE) {
+            state = State.SPEED_UP;
+            launches = 0;
+            inToleranceTimer.reset();
+            inToleranceTimer.reset();
+        }
+    }
+
+
+
+    public State getState() {
+        return state;
+    }
+
+    public void update(boolean b) {
+        switch (state) {
+            case IDLE:
+                chipMotor.setPower(0);
+                lServo.setPower(0);
+                rServo.setPower(0);
+                break;
+
+            case SPEED_UP:
+                chipMotor.setVelocity(TARGET_RPM);
+                boolean chipInTol = Math.abs(TARGET_RPM - getChipRPM()) <= RPM_TOLERANCE;
+
+                if (chipInTol) {
+                    if (inToleranceTimer.milliseconds() >= RPM_IN_RANGE_TIME) {
+                        // Ready to feed
+                        lServo.setPower(feedPower);
+                        rServo.setPower(-feedPower);
+                        state = State.FEED;
+                        feedTimer.reset();
+                    }
+                } else {
+                    inToleranceTimer.reset();
+                }
+                break;
+
+            case FEED:
+
+                if (feedTimer.milliseconds() >= FEED_TIME) {
+                    lServo.setPower(0);
+                    rServo.setPower(0);
+                    launches++;
+                    recoveryTimer.reset();
+                    state = State.RECOVER;
+                }
+                break;
+
+            case RECOVER:
+                if (recoveryTimer.milliseconds() >= MIN_TIME_BETWEEN_LAUNCHES) {
+                    if (launches >= targetLaunches) {
+                        stop();
+                    }
+                    else {
+                        state = State.SPEED_UP;
+                        inToleranceTimer.reset();
+                    }
+                }
+                break;
+        }
+    }
+}
